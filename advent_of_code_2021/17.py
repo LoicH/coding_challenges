2959a7d9112e8e23e5a7f5cef013846edf07f1c7
Loïc H <LoicH@users.noreply.github.com>
2021-12-17 23:51:47 +0100

    explain

--- advent_of_code_2021/17.py
+++ advent_of_code_2021/17.py
@@ -54,14 +54,28 @@ def reach_target(x, y, x_min, x_max, y_min, y_max, max_steps=1000):
 
 # Part 2
 def part2(x_min, x_max, y_min, y_max):
+    # For our probe to have a chance to ever reach the target zone,
+    # The horizontal speed x must cannot be too small, because
+    # x + (x-1) + ... + 1 >= x_min
+    # i.e. sum of k from 1 to x >= x_min
+    # i.e. x * (x+1) / 2 >= x_min
+    # therefore x^2 + x - 2 * x_min >= 0
+    # therefore x >= (sqrt(8*xmin + 1) - 1) / 2
     x_lower = math.ceil((math.sqrt(1 + 8 * x_min) - 1) / 2)
+    # And if x > x_max, the first step will overshoot the target zone
     x_upper = x_max
+    # If y_max<0, then inital vertical velocity is must be lower than |y_min|,
+    # because if the initial dy is |y_min| + 1 or more,
+    # at step number dy we reach the maximum height
+    # and then at step 2*dy we reach y=0 again, with a vertical velocity of -dy
+    # therefore at step 2*dy + 1 we will reach y = -dy - 1, which is greater than y_min
+    # So we jumped over the target zone
+    y_upper = -y_min
+    # And if dy < y_min, the first step will miss the target zone
     y_lower = y_min
-    y_upper = -y_min  # Works only if the target area is below the submarine,
-    # i.e. if y_min < y_max < 0
     n_values = 0
-    for x, y in itertools.product(
-        range(x_lower, x_upper + 1), range(y_lower, y_upper + 1)
+    for x, y in tqdm(
+        itertools.product(range(x_lower, x_upper + 1), range(y_lower, y_upper + 1))
     ):
         if reach_target(x, y, x_min, x_max, y_min, y_max):
             n_values += 1
