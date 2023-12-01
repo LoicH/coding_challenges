96c2a5d05fd9f0161357e3e6517d06d0fc6569ce
LoicH <loic.herbelot@free.fr>
2023-12-01 16:46:30 +0100

    comments

--- advent_of_code_2023/day1/src/main.rs
+++ advent_of_code_2023/day1/src/main.rs
@@ -15,33 +15,34 @@ const DIGITS: [&str; 9] = [
 ];
 
 fn main() -> std::io::Result<()> {
-    // tests
+    // Testing digit extraction with single numbers
     assert_eq!(extract_digit("two"), Some(2));
-    assert_eq!(extract_digit("two1"), Some(2));
-    assert_eq!(extract_digit("two1nine"), Some(2));
     assert_eq!(extract_digit("eight"), Some(8));
+    // Testing digit extraction with multiple numbers
+    assert_eq!(extract_digit("two1"), Some(2));
     assert_eq!(first_and_last_digit_part2("twonine"), 29);
+    assert_eq!(extract_digit("two1nine"), Some(2));
     assert_eq!(first_and_last_digit_part2("two1nine"), 29);
-    assert_eq!(
-        first_and_last_digit("lpbjvpbtdfvtxtdvkpjs7qrvddkzmjtlqtg"),
-        77
-    );
+    // Testing when only one digit is in the string
+    assert_eq!(first_and_last_digit("abc7abc"), 77);
 
-    println!("Testing on example...");
+    println!("Testing the full example...");
     let n = part2(EXAMPLE2);
-    println!("Result: {n}");
     assert_eq!(n, 281);
+    println!("Result: {n}, as expected.");
 
     // part 2
-    println!("Reading input file...");
+    println!("Reading the input file...");
     let mut file = File::open("input.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
+    println!("Computing the result...");
     let res = part2(&contents);
     println!("Result: {res}");
     Ok(())
 }
 
+// Main function used for part 1
 fn first_and_last_digit(line: &str) -> u32 {
     let mut a: Option<u32> = None;
     let mut b: Option<u32> = None;
@@ -51,13 +52,13 @@ fn first_and_last_digit(line: &str) -> u32 {
                 Some(_) => a,
                 _ => c.to_digit(10),
             };
+            // This will cause the function to panic if `line` contains only one digit
             b = match a {
                 Some(_) => c.to_digit(10),
                 _ => b,
             };
         }
     }
-    println!("a = {a:?}");
     a.unwrap() * 10 + b.unwrap()
 }
 
@@ -67,6 +68,7 @@ fn part1(s: &str) -> u32 {
     nums.sum()
 }
 
+// Used in part 2 to return a number if the string starts with a number (in digit or written form)
 fn extract_digit(s: &str) -> Option<u32> {
     println!("In extract_digit, s={s}");
     let c = s.chars().next().unwrap();
@@ -83,6 +85,7 @@ fn extract_digit(s: &str) -> Option<u32> {
     }
 }
 
+// Main function used for part 2
 fn first_and_last_digit_part2(line: &str) -> u32 {
     let mut i: usize = 0;
     let mut a: Option<u32> = None;
@@ -92,19 +95,15 @@ fn first_and_last_digit_part2(line: &str) -> u32 {
         let sub: &str = line.get(i..).expect("no line??");
         match extract_digit(sub) {
             Some(val) => {
-                println!("val={val:?}");
                 b = Some(val.try_into().unwrap());
                 a = match a {
                     Some(_) => a,
                     _ => Some(val.try_into().unwrap()),
                 };
             }
-            None => {
-                println!("Nothing found, going on.");
-            }
+            None => {}
         }
         i = i + 1;
-        println!("i={i}");
     }
 
     a.unwrap() * 10 + b.unwrap()
