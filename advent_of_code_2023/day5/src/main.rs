e035c795baa507ec1e9dd7d183cead16a4b1066e
LoicH <loic.herbelot@free.fr>
2023-12-08 18:38:04 +0100

    part2 wip

--- advent_of_code_2023/day5/src/main.rs
+++ advent_of_code_2023/day5/src/main.rs
@@ -51,8 +51,8 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
     println!("Computing the result...");
     let res = part1(&contents);
     println!("Part 1: {res}");
-    // let res = part2(&contents);
-    // println!("Part 2: {res}");
+    let res = part2(&contents);
+    println!("Part 2: {res}");
     Ok(())
 }
 
@@ -95,10 +95,8 @@ fn parse_input(s: &str) -> (Vec<u32>, Vec<Vec<[u32; 3]>>) {
 
 fn apply_map(n: u32, mappings: &Vec<[u32; 3]>) -> u32 {
     let mut val: Option<u32> = None;
-    println!("n = {n}");
     for [dest, src, range] in mappings {
         if src <= &n && n - src <= *range {
-            println!("Matches mapping dest = {dest}, src = {src}, range = {range}");
             val = Some(n - src + dest);
         }
     }
@@ -110,14 +108,60 @@ fn apply_map(n: u32, mappings: &Vec<[u32; 3]>) -> u32 {
 
 fn part1(s: &str) -> u32 {
     let (seeds, all_mappings) = parse_input(s.trim());
-    println!("Seeds = {seeds:?}");
     let mut new_vals: Vec<u32> = seeds;
     for mappings in all_mappings {
         new_vals = new_vals.iter().map(|n| apply_map(*n, &mappings)).collect();
     }
     *new_vals.iter().min().unwrap()
 }
-// fn part2(s: &str) -> u32 {
-//     2
-// TODO Map intervals! Good luck
-// }
+
+fn interval_intersection(interval: &(u32, u32), mapping: &[u32; 3]) -> Vec<(u32, u32)> {
+    let (start_a, range_a) = interval;
+    let [dest_b, start_b, range_b] = mapping;
+
+    vec![(3, 4)]
+}
+
+fn min_range(intervals: &Vec<(u32, u32)>) -> u32 {
+    let min: Option<u32> = None;
+    for (a, _) in intervals {
+        match min {
+            None => Some(a),
+            Some(v) => Some(std::cmp::min(a, &v)),
+        };
+    }
+    min.unwrap()
+}
+
+fn seeds_to_interval(seeds: Vec<u32>) -> Vec<(u32, u32)> {
+    let mut intervals: Vec<(u32, u32)> = Vec::new();
+    // Computing intervals (start, range) from the first line of the input
+    let mut i = 0;
+    (while i < seeds.len() {
+        intervals.push((seeds[i], seeds[i + 1]));
+        i = i + 2;
+    });
+    intervals
+}
+
+// We have our starting intervals which consist of (starting_number, number_of_elements)
+// We have all of the mappings for all categories
+// We can iterate over the categories
+// For each mappings in all_mappings
+// We can iterate over the cross product of the intervals vector and mappings vector
+// Then sort the resulting intervals
+// Then reassemble the intervals that are next to each other
+fn part2(s: &str) -> u32 {
+    let (seeds, all_mappings) = parse_input(s.trim());
+    let intervals = seeds_to_interval(seeds);
+    (for mappings in all_mappings {
+        let mut new_intervals: Vec<(u32, u32)> = Vec::new();
+        for interval in &intervals {
+            for mapping in &mappings {
+                new_intervals.extend(interval_intersection(interval, mapping));
+            }
+        }
+        let intervals = new_intervals;
+    });
+    min_range(&intervals)
+}
