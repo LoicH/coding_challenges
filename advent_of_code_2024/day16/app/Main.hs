8ee578ed320bf79eb4467812dc7ca58644c8f326
LoicH <loic.herbelot@free.fr>
2024-12-27 15:50:49 +0100

    todo debug

--- advent_of_code_2024/day16/app/Main.hs
+++ advent_of_code_2024/day16/app/Main.hs
@@ -3,12 +3,15 @@ module Main where
 import Data.Char(digitToInt, intToDigit)
 import qualified Data.Set as S 
 import qualified Data.List as L
+import qualified Data.SortedList as SL
 import qualified Data.Map as M
+import Data.Map((!),(!?))
 import Text.Printf(printf)
 import Control.Monad(unless)
-import Debug.Trace(trace)
+import Debug.Trace(trace,traceShow)
 
 type Coord = (Int, Int)
+type Node = (Coord, Coord)
 
 parseLine :: String -> Int -> [Coord] 
 parseLine l i = do 
@@ -22,13 +25,13 @@ search ls c = do
         Just j = L.findIndex ((==) c) (ls!!i)
      in (i,j)
 
-parse :: String -> ([Coord], Coord, Coord)
+parse :: String -> ([Coord], Node, Coord)
 parse content = do 
     let ls = lines content
-        walkables = L.foldl (\accu (l,i) -> accu ++ parseLine l i)[] (zip (lines content) [0..])
+        walkables = L.foldl (\accu (l,i) -> accu ++ parseLine l i) [] (zip (lines content) [0..])
         s = search ls 'S'
         e = search ls 'E'
-     in (walkables, s, e)
+     in (walkables, (s,(0,1)), e)
 
 turns :: Coord -> [Coord]
 turns (di,dj) = case (di,dj) of 
@@ -38,27 +41,66 @@ turns (di,dj) = case (di,dj) of
 
 dirs = [(0,1), (0,-1), (-1,0), (1,0)] :: [Coord]
 
-type Graph = M.Map (Coord, Coord) [(Coord, Coord,Int)]
+type Graph = M.Map Node [(Node,Cost)]
 
-addNeighbours :: (Coord, Coord) -> a -> Graph -> Graph
+addNeighbours :: Node -> a -> Graph -> Graph
 addNeighbours k@((i,j), d@(di,dj)) _ graph = do 
-            let singleton = M.singleton k (if ((i+di,j+dj), d) `M.member` graph then [((i+di, j+dj), d, 1)] else []) 
+            let singleton = M.singleton k (if ((i+di,j+dj), d) `M.member` graph then [(((i+di, j+dj), d), Some 1)] else []) 
              in M.unionWith (++) graph singleton 
 
 buildGraph :: [Coord] -> Graph
 buildGraph cs = do 
     -- Empty map of all nodes & directions
-    let graph = M.fromList [((c,d),[(c,t,90) | t <- turns d]) | c <- cs, d <- dirs]
+    let graph = M.fromList [((c,d),[((c,t),Some 90) | t <- turns d]) | c <- cs, d <- dirs]
     -- For each node/direction: add its neighbours: two turns + an eventual forward
     -- M.foldrWithKey : (k -> a -> b -> b) -> b -> Map k a -> b
     -- b = Graph = Map k a 
-    -- k = (Coord, Coord)
+    -- k = Node
     -- a = [(Coord, Coord, Int)]
         graphWithNeighbours = M.foldrWithKey addNeighbours graph graph 
      in graphWithNeighbours
 
     {- fold function k(c,d) _ graph = M.unionWith (++) graph (M.singleton k (if c+d `member` graph then [(c+d, d, 1)] else []))
     -}
+data Cost = Infinity | Some Int deriving (Show, Eq)
+instance Ord Cost where 
+    Infinity `compare` Infinity = EQ
+    _ `compare` Infinity = LT
+    Infinity `compare` _ = GT
+    (Some a) `compare` (Some b) = a `compare` b
+
+instance Num Cost where 
+    Some a + Some b = Some (a+b)
+    _ + _ = Infinity
+
+type Queue = SL.SortedList (Cost, Node)
+
+visit :: Graph -> Queue -> M.Map Node Cost -> Coord -> Cost
+visit graph queue dist end = case SL.uncons queue of 
+        Nothing -> undefined
+        Just (u, us) -> let (d,current) = traceShow (u) u 
+                         in if fst current == end
+                            then traceShow "end!" d 
+                            else let unvisited = S.fromList . SL.fromSortedList . SL.map snd $ us :: S.Set Node
+                                     neighbCosts = [(v,c) | (v,c) <- graph!current, v `S.member` unvisited] :: [(Node,Cost)]
+                                     alts = M.fromList [(v, d+c) | (v,c) <- neighbCosts] :: M.Map Node Cost
+                                     newDist = M.unionWith min dist alts :: M.Map Node Cost
+                                     -- For all v in us, dist[v] = newDist!v
+                                     newQueue = SL.map (\(_,v) -> (newDist!v, v)) us :: Queue
+
+                                  in visit graph newQueue newDist end
+
+dijkstra :: Graph -> Node -> Coord -> Cost 
+dijkstra graph start end = do 
+    let nodes = M.keys graph 
+        origDist = M.fromList $ zip nodes (repeat Infinity)
+        dist = M.adjust (\_ -> Some 0) start origDist :: M.Map Node Cost
+        queue = SL.toSortedList [(d,n) | (n,d) <- M.toList dist]
+        n = traceShow queue visit graph queue dist end
+     in n
+
+                         
+
 
 part1 :: String -> Int
 part1 content = 0
@@ -72,7 +114,10 @@ main = do
     test_input <- readFile "test_input.txt"
     full_input <- readFile "input.txt"
     -- Tests 
-
+    let (coords, start, end) = parse test_input
+        graph = buildGraph coords 
+    print graph
+    print (dijkstra graph start end)
     print "Tests passed!"
 
     -- Part 1
