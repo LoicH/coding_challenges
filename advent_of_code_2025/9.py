f9224f6438ba697fba741e0c4718872b18c4015c
Loïc H. <ext.loic.herbelot@sncf.fr>
2025-12-14 16:26:02 +0100

    min max outside function

--- advent_of_code_2025/9.py
+++ advent_of_code_2025/9.py
@@ -28,13 +28,10 @@ def sides(data):
 
 
 def rectangle_doesnt_intersect_rokil(
-    a: tuple, b: tuple, horizontal_sides: dict, vertical_sides: dict
+    min_i, max_i, min_j, max_j, horizontal_sides: dict, vertical_sides: dict
 ) -> bool:
     """Check if no side of the polygon intersects with this rectangle
     This DOES NOT check if the opposite corners (a and b) of the rectangle are red"""
-    min_i, max_i = min(a[0], b[0]), max(a[0], b[0])
-    min_j, max_j = min(a[1], b[1]), max(a[1], b[1])
-
     # Checking if a vertical side intersects the rectangle
     for i in range(min_i + 1, max_i):
         sides = vertical_sides.get(i, [])  # list of tuples (j1, j2)
@@ -82,7 +79,9 @@ def part2(red_tiles):
 
     # Let's iterate over the biggest rectangles, and find the first that is valid
     for a, b, area in tqdm.tqdm(all_rectangles):
-        if rectangle_doesnt_intersect_rokil(a, b, horiz, verti):
+        min_i, max_i = min(a[0], b[0]), max(a[0], b[0])
+        min_j, max_j = min(a[1], b[1]), max(a[1], b[1])
+        if rectangle_doesnt_intersect_rokil(min_i, max_i, min_j, max_j, horiz, verti):
             print(f"Big valid rectangle found with corners {a} and {b}")
             return area
 
@@ -91,9 +90,12 @@ import timeit
 
 horiz, verti = sides(puzzle_red_tiles)
 a, b = (4615, 66437), (94737, 50322)
+
+min_i, max_i = min(a[0], b[0]), max(a[0], b[0])
+min_j, max_j = min(a[1], b[1]), max(a[1], b[1])
 print(
     timeit.timeit(
-        lambda: rectangle_doesnt_intersect_rokil(a, b, horiz, verti), number=100
+        lambda: rectangle_doesnt_intersect_rokil(min_i, max_i, min_j, max_j, horiz, verti), number=100
     )
 )
 # 0.8713879999704659
