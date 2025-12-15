1ca7cd7122375617b8939133faa9e618bb63cfbb
LoicH <loic.herbelot@free.fr>
2025-12-15 09:17:02 +0100

    part 1 done, part 2 too slow

--- advent_of_code_2025/10.py
+++ advent_of_code_2025/10.py
@@ -1,14 +1,32 @@
 from functools import reduce
 import operator
+import bisect
+from os import curdir
+import tqdm
 
 
-def read_line(txt: str):
+def read_line(line: str):
     # Convert each diagram to a number (".#.." = 2)
-    # Convert every wiring schematics to a number
-    # Discard the rest
-    pass
+    words = line.strip().split()
+    diagram = words[0][1:-1]
+    # '.#..' = 2
+    diagram_value = sum(2**i for (i, c) in enumerate(diagram) if c == "#")
+
+    schematics = [[int(c) for c in w[1:-1].split(",")] for w in words[1:-1]]
+
+    joltage_goal = [int(n) for n in words[-1][1:-1].split(",")]
+    return diagram_value, schematics, joltage_goal
+
+
+assert read_line("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}") == (
+    6,
+    [[3], [1, 3], [2], [2, 3], [0, 2], [0, 1]],
+    [3, 5, 4, 7],
+)
+
+
 def read_data(txt: str):
-    pass
+    return [read_line(l) for l in txt.strip().split("\n")]
 
 
 raw_input_example = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
@@ -17,26 +35,94 @@ raw_input_example = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
 """
 
 
-def part1(data) -> int:
-    # For every machine:
-    # We'll start with a queue of [0], then iterate over every button, 
-    # until we match the diagram of this machine, 
+def match_lights(diagram_value, schematics: list) -> int:
+    # We'll start with a queue of [0], then iterate over every button,
+    # until we match the diagram of this machine,
     # being careful not to compute multiple times the same diagram
     # For instance in the 1st machine, pushing the (3) and (2) buttons is the same as pushing (2,3)
-    return 0
+    schematics_values = [sum(2**n for n in s) for s in schematics]
+    queue = [(0, 0)]  # diagram value, number of buttons pushed
+    already_seen = {0}
+    for _ in range(2 ** len(schematics_values)):
+        current_light, steps = queue[0]
+        queue = queue[1:]
+        for button in schematics_values:
+            new_light = current_light ^ button
+            if new_light == diagram_value:
+                return steps + 1
+            elif new_light in already_seen:
+                continue
+            already_seen.add(new_light)
+            queue.append((new_light, steps + 1))
+    raise ValueError(f"Can't match lights with value {diagram_value}")
+
+
+assert match_lights(6, [[3], [1, 3], [2], [2, 3], [0, 2], [0, 1]]) == 2
+
+
+def part1(machines) -> int:
+    return sum(match_lights(d, s) for d, s, _ in machines)
+
 
 input_example = read_data(raw_input_example)
-assert part1(input_example) == 0, "Error: part 1 on example"
+assert part1(input_example) == 7, "Error: part 1 on example"
 
 input_data = read_data(open("input_10.txt").read())
 print(f"Result on puzzle input: {part1(input_data)}")
 
 
-def part2(inp):
-    return 0
+def dist(a: list, b: list) -> int:
+    return sum(abs(i - j) for i, j in zip(a, b))
+
+
+def match_joltage(goal: list, buttons: list) -> int:
+    start = [0] * len(goal)
+    queue_to_visit = [(start, 0, dist(start, goal))]
+    already_seen = {tuple(start)}
+    buttons_binary = []
+    for b in buttons:
+        new_button = [0] * len(goal)
+        for i in b:
+            new_button[i] = 1
+        buttons_binary.append(new_button)
+    while queue_to_visit:  # for _ in range(max(goal) * len(buttons)):
+        current_joltage, steps, cur_dist = queue_to_visit[0]
+        print(
+            f"{len(queue_to_visit)} elements to check, already seen {len(already_seen)} joltages, current distance = {cur_dist}"
+        )
+        queue_to_visit = queue_to_visit[1:]
+        for b in buttons_binary:
+            new_joltage = [n + b[i] for i, n in enumerate(current_joltage)]
+            if new_joltage == goal:
+                return steps + 1
+            # Stop early if we get one joltage requirement that is too big
+            too_big = any(new_joltage[i] > req for i, req in enumerate(goal))
+            if too_big:
+                continue
+            t = tuple(new_joltage)
+            if t in already_seen:
+                continue
+            already_seen.add(t)
+            elt_to_add = (new_joltage, steps + 1, dist(new_joltage, goal))
+            bisect.insort(queue_to_visit, elt_to_add, key=lambda t: t[2])
+    raise ValueError(f"Can't reach {goal=}")
+
+
+assert (
+    match_joltage([3, 5, 4, 7], [[3], [1, 3], [2], [2, 3], [0, 2], [0, 1]]) == 10
+), "Error on the first line of example"
+assert match_joltage([0, 100, 5, 5, 0], [[0, 1], [1], [2], [3], [4]]) == 110
+
+
+def part2(machines):
+    total = 0
+    for _, b, j in tqdm.tqdm(machines):
+        total += match_joltage(j, b)
+    return total
+
 
 test_cases_part_2 = {
-    raw_input_example: 0,
+    raw_input_example: 33,
 }
 
 print("Computing test cases for part 2...")
